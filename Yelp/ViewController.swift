292a36857f1c3ab96ffb331fe6c303cfac479fc1
Nabib El-Rahman <nabib.el-rahman@dailymotion.com>
2014-09-21 00:49:11 -0700

    Update token to personal tokens

--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -12,10 +12,10 @@ class ViewController: UIViewController {
     var client: YelpClient!
     
     // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
-    let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
-    let yelpConsumerSecret = "33QCvh5bIF5jIHR5klQr7RtBDhQ"
-    let yelpToken = "uRcRswHFYa1VkDrGV6LAW2F8clGh5JHV"
-    let yelpTokenSecret = "mqtKIxMIR4iBtBPZCmCLEb-Dz3Y"
+    let yelpConsumerKey = "nUjddnVqhLB-8g3XFxLa7w"
+    let yelpConsumerSecret = "MBA5pputws_K-omJ8lCkc9rgjxE"
+    let yelpToken = "McUoPa8XkG8pVBoyVFgBNpqFmtzahRqW"
+    let yelpTokenSecret = "VxKYwD71miWVxIjbZ_-pNvETgKc"
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
